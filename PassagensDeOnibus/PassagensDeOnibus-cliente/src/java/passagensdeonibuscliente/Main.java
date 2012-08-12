ce75adb246be32048166d6fabaf3f5915c1073f9
Eduardo Souza <duezehss@gmail.com>
2012-08-12 19:57:31 -0300

    Criação de entidades
    Cópia de um projeto anitgo e depois criação das entidades.

---
+++ PassagensDeOnibus/PassagensDeOnibus-cliente/src/java/passagensdeonibuscliente/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package passagensdeonibuscliente;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
